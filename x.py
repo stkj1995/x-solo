40b6558c622f4f665e32f55b235b8dc61a963c26
Sophie <soph1155@stud.kea.dk>
2025-12-07 16:47:36 +0100

    edit ...

--- x.py
+++ x.py
@@ -97,6 +97,19 @@ def db():
         print(e, flush=True)
         raise Exception("Database under maintenance", 500)
 
+########################
+import mysql.connector
+
+def db():
+    db_conn = mysql.connector.connect(
+        host="teinvig.mysql.eu.pythonanywhere-services.com",
+        user="teinvig",
+        password="datapassword123",
+        database="teinvig$default"
+    )
+    cursor = db_conn.cursor(dictionary=True)
+    return db_conn, cursor
+
 ##############################
 def no_cache(view):
     @wraps(view)
