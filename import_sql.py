be310d563718aece67a9744e22f8aeb1155be3d7
Sophie <soph1155@stud.kea.dk>
2025-12-08 09:11:59 +0100

    comment out import_sql.py

--- import_sql.py
+++ import_sql.py
@@ -1,22 +1,22 @@
-import mysql.connector
+# import mysql.connector
 
-conn = mysql.connector.connect(
-    host="teinvig.mysql.eu.pythonanywhere-services.com",
-    user="teinvig",
-    password="datapassword123",
-    database="teinvig$default"
-)
+# conn = mysql.connector.connect(
+#     host="teinvig.mysql.eu.pythonanywhere-services.com",
+#     user="teinvig",
+#     password="datapassword123",
+#     database="teinvig$default"
+# )
 
-cursor = conn.cursor()
+# cursor = conn.cursor()
 
-with open("/home/teinvig/x-solo/x_clean.sql", "r") as f:
-    sql = f.read()
+# with open("/home/teinvig/x-solo/x_clean.sql", "r") as f:
+#     sql = f.read()
 
-for statement in sql.split(";"):
-    if statement.strip():
-        cursor.execute(statement)
+# for statement in sql.split(";"):
+#     if statement.strip():
+#         cursor.execute(statement)
 
-conn.commit()
-conn.close()
-print("SQL imported successfully!")
+# conn.commit()
+# conn.close()
+# print("SQL imported successfully!")
 
